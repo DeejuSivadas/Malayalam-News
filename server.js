916e71676dbee6a2f5978889e692965c931c2f08
Deeju Sivadas <deeju@MacBook-Pro.local>
2026-02-18 20:13:40 +1100

    Bind server to 0.0.0.0 for Railway health checks

--- server.js
+++ server.js
@@ -497,6 +497,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on 0.0.0.0:${PORT}`);
 });
